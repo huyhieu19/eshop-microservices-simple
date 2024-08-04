0406cc8d7f277e39720078055048b9929bb992d3
HuyHieu <huyhieu.hust2855@gmail.com>
2024-08-04 21:59:41 +0700

    Create Handler and Endpoint for catalog api

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS;
+
+public interface IQueryHandler<in TCommand> : ICommandHandler<TCommand, Unit>
+    where TCommand : ICommand<Unit>
+{
+}
+
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
