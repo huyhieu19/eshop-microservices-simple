a1f4b1d781e1c81581cb47bf28a896a6c08a74f3
HuyHieu <huyhieu.hust2855@gmail.com>
2024-08-12 00:21:57 +0700

    Add Discount Service

---
+++ src/services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DiscountContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+            new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+            );
+    }
+
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+}
