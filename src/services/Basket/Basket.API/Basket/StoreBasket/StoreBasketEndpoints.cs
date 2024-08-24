2233c3c425bca9d6b8cb819568fa12e686ce59af
HuyHieu <huyhieu.hust2855@gmail.com>
2024-08-25 00:47:22 +0700

    Containerize and Orchestrate Ordering microservices

--- src/services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ src/services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,8 +1,4 @@
-﻿
-using Mapster;
-using MediatR;
-
-namespace Basket.API.Basket;
+﻿namespace Basket.API.Basket;
 
 public record StoreBasketRequest(ShoppingCart Cart);
 
@@ -12,20 +8,19 @@ public class StoreBasketEndpoints : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/basket", async (StoreBasketRequest request, ISender sender) =>
-        {
-            var command = request.Adapt<StoreBasketCommand>();
+        app.MapPost("/basket", StoreBasket)
+            .WithName("StoreBasket")
+            .Produces<StoreBasketResponse>(statusCode: 201)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Store Basket")
+            .WithDescription("Store Basket");
+    }
 
-            var result = await sender.Send(command);
-
-            var response = result.Adapt<StoreBasketResponse>();
-
-            return Results.Created($"/basket/{response.UserName}", response);
-
-        }).WithName("StoreBasket")
-        .Produces<StoreBasketResponse>(statusCode: 201)
-        .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithSummary("Store Basket")
-        .WithDescription("Store Basket");
+    private async Task<IResult> StoreBasket(StoreBasketRequest request, ISender sender)
+    {
+        var command = request.Adapt<StoreBasketCommand>();
+        var result = await sender.Send(command);
+        var response = result.Adapt<StoreBasketResponse>();
+        return Results.Created($"/basket/{response.UserName}", response);
     }
 }
